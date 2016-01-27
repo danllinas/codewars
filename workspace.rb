8237f45f1d80160c6f9b1b1d3425e2c6111d02d2
Daniel Llinas <danllinas@gmail.com>
2016-01-27 10:36:54 -0500

    Grading studnet work.

--- workspace.rb
+++ workspace.rb
@@ -1,24 +1,24 @@
-def max(number_1, number_2)
-  if number_1 > number_2
-    number_1
-  elsif number_1 < number_2
-    number_2
-  elsif number_1 = number_2
-    number_1
-  end
+class Square
+    @@side_length
+
+    def initialize(length)
+        @@side_length=length
+        return @@side_length
+    end
+
+    def area
+        return @@side_length * @@side_length
+    end
+
+    def side_length=(length)
+        @@side_length=length
+    end
+
+    def side_length
+        puts @@side_length
+        return @@side_length
+    end
 end
 
-def test
-  puts max(2,3) == 3
-  puts max(3, 3)
-  # # puts max(nil,1)
-  # puts max(1.1,100) == 100
-  # puts max(99999999999999999,4)
-  # puts max(true, 0)
-  # puts max({},4)
-  # puts max(4,[])
-  # p max([],5)
-  # p max([1,2,3],5)
-  # puts max("","")
-end
-test()
+s = Square.new(2)
+p s.area
